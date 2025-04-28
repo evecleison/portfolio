a2777f49835df8f5306642c63337352177700b94
Evecleison <evecleisonce@gmail.com>
2025-04-28 01:39:46 -0300

    build: update for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/portfolio/' : '/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'docs'
